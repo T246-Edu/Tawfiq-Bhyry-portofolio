f91f6d7c346d44bbd4933f753e4175819bf9d241
T246-Edu <tawfik.2419010@stemdakahlia.moe.edu.eg>
2021-11-26 20:33:34 +0200

    Updates

--- precache-manifest.d63791005c43011bb49e29e7ed21f9b2.js
+++ precache-manifest.d63791005c43011bb49e29e7ed21f9b2.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "89f48bab25bcab825a0374a776fc82b3",
+    "revision": "0d23f4629b0579b42ef57937601a4728",
     "url": "/Tawfiq-Bhyry-portofolio/index.html"
   },
   {
-    "revision": "c0f578b6f5f69b87bf5b",
+    "revision": "a7983c4ac51dc7c400e8",
     "url": "/Tawfiq-Bhyry-portofolio/static/css/2.d64471e2.chunk.css"
   },
   {
-    "revision": "f0cc5d5764f91d73f08a",
+    "revision": "0a1428ba76a5f67e4b7d",
     "url": "/Tawfiq-Bhyry-portofolio/static/css/main.40f0e6cc.chunk.css"
   },
   {
-    "revision": "c0f578b6f5f69b87bf5b",
-    "url": "/Tawfiq-Bhyry-portofolio/static/js/2.4524cd39.chunk.js"
+    "revision": "a7983c4ac51dc7c400e8",
+    "url": "/Tawfiq-Bhyry-portofolio/static/js/2.a39e7c15.chunk.js"
   },
   {
     "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
-    "url": "/Tawfiq-Bhyry-portofolio/static/js/2.4524cd39.chunk.js.LICENSE.txt"
+    "url": "/Tawfiq-Bhyry-portofolio/static/js/2.a39e7c15.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "f0cc5d5764f91d73f08a",
-    "url": "/Tawfiq-Bhyry-portofolio/static/js/main.30ba9ab4.chunk.js"
+    "revision": "0a1428ba76a5f67e4b7d",
+    "url": "/Tawfiq-Bhyry-portofolio/static/js/main.b4a532ba.chunk.js"
   },
   {
     "revision": "ba24a95d75348fd5bd56",
